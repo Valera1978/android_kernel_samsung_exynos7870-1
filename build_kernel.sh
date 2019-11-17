4f232c8b28e59e6a850720e715fc7e3350e8994a
Valera1978 <valera1978@tut.by>
2019-11-17 16:50:54 +0300

    fix build

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,13 @@
+
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=~/android/kernel/toolchain/gcc-linaro-7.4.1/bin/aarch64-linux-gnu-
-export ANDROID_MAJOR_VERSION=p
+export ANDROID_MAJOR_VERSION=o
+export CROSS_COMPILE=~/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
-make O=./out a3y17ltexc_00_defconfig
-make O=./out -j16
+rm -rf output
+mkdir output
+
+make -C $(pwd) O=output lineage_j7xelte_defconfig
+
+make -C $(pwd) O=output -j16
